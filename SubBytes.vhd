b446e06d0c3f33762cdfa86322f3b767ecbb628b
FrBrenno <72556312+FrBrenno@users.noreply.github.com>
2023-12-12 23:16:50 +0100

    SubBytes working + SubBytes testbench + simulation files

--- SubBytes.vhd
+++ SubBytes.vhd
@@ -10,7 +10,6 @@ entity SubBytes is
 end entity SubBytes;
 
 architecture arch_SubBytes of SubBytes is
-
     -- Component declaration
     component S_box is
         port (
@@ -19,66 +18,23 @@ architecture arch_SubBytes of SubBytes is
         );
     end component;
     
-    -- Type declaration
-    type Matrix is array (0 to 3, 0 to 3) of std_logic_vector(7 downto 0);
-
     -- Signal declaration
-    signal temp_byte : std_logic_vector(7 downto 0);
-    signal byte_out : std_logic_vector(7 downto 0);
-    
-    -- Function declaration
-    function hexaToMatrix(input_hex : std_logic_vector(127 downto 0)) return Matrix is
-        variable result : Matrix := (
-                                (x"00", x"00", x"00", x"00"),
-                                (x"00", x"00", x"00", x"00"),
-                                (x"00", x"00", x"00", x"00"),
-                                (x"00", x"00", x"00", x"00")
-                              );
-        variable index : integer := 0;    
-        begin
-            for col_k in 0 to 3 loop
-                for row_k in 0 to 3 loop   
-                    if index < input_hex'length then
-                        result(row_k,col_k) := std_logic_vector(unsigned(input_hex((7+index*7) downto (0+index*7))));
-                        index := index + 1;
-                    end if;
-                end loop;
-            end loop;
-        return result;    
-    end hexaToMatrix;
-
-    function matrixToHexa(input_matrix : Matrix) return std_logic_vector is
-        variable result : std_logic_vector(127 downto 0) := (others => '0');
-        variable index : integer := 0;    
-        begin
-            for col_k in 0 to 3 loop
-                for row_k in 0 to 3 loop   
-                    result((7+index*7) downto (0+index*7)) := input_matrix(row_k,col_k);
-                    index := index + 1;
-                end loop;
-            end loop;
-        return result;    
-    end matrixToHexa;
-    
+    signal temp_byte: std_logic_vector(7 downto 0);
+    signal byte_out: std_logic_vector(7 downto 0);
 
 begin
-    S_box_inst: S_box port map (
+    S_box_inst: S_box port map(
         BYTE_IN => temp_byte,
         BYTE_OUT => byte_out
     );
 
-    subBytes_process: process
-        variable row, col: integer;
-        variable temp_matrix : Matrix := hexaToMatrix(aMatrix);
+    process
     begin
-        for row in 0 to 3 loop
-            for col in 0 to 3 loop
-                temp_byte <= temp_matrix(row,col);
-                wait for 5 ns;
-                temp_matrix(row, col) := byte_out;
-                wait for 5 ns;
-            end loop;
-        end loop;
-        bMatrix <= matrixToHexa(temp_matrix);
+        for i in 0 to 15 loop
+            temp_byte <= aMatrix(i*8+7 downto i*8);
+            wait for 1 ns;
+            bMatrix(i*8+7 downto i*8) <= byte_out;
+        end loop;        
     end process;
+
 end architecture arch_SubBytes;
--- SubBytes_tb.vhd
+++ SubBytes_tb.vhd
@@ -24,22 +24,22 @@ begin
     simulation_rounds: process
     begin
         plain_text <= x"40BFABF406EE4D3042CA6B997A5C5816";
-        wait for 10 ns;
+        wait for 20 ns;
         plain_text <= x"F265E8D51FD2397BC3B9976D9076505C";
-        wait for 10 ns;
+        wait for 20 ns;
         plain_text <= x"FDF37CDB4B0C8C1BF7FCD8E94AA9BBF8";
-        wait for 10 ns;
+        wait for 20 ns;
         plain_text <= x"ACD1EC9CA242E2C31F690F7AB704B90F";
-        wait for 10 ns;
+        wait for 20 ns;
         plain_text <= x"A2616E5F44A54D39C029E20092B764E9";
-        wait for 10 ns;
+        wait for 20 ns;
         plain_text <= x"2C4AF31432C3EFC9C8A9B87B252ECDA7";
-        wait for 10 ns;
+        wait for 20 ns;
         plain_text <= x"CD4DC0137EB3BA1993B939FF2BD3BCF7";
-        wait for 10 ns;
+        wait for 20 ns;
         plain_text <= x"E26DBB7D40D22134E3B7FDA26B9B077C";
-        wait for 10 ns;
+        wait for 20 ns;
         plain_text <= x"41D7C6537D669140DD2F179D02ACC51B";
-        wait for 10 ns;       
+        wait for 20 ns;       
     end process;
 end architecture arch_SubBytes_tb;
