e94f0b6ff1ffac883e1a9825964ae343033d393e
FrBrenno <72556312+FrBrenno@users.noreply.github.com>
2023-12-12 15:23:00 +0100

    SubBytes + SubBytes_tb + simulation
    It is not working

--- SubBytes.vhd
+++ SubBytes.vhd
@@ -1,41 +1,84 @@
--- FILEPATH: /c:/Users/brenn/Vivado/elech409_project/SubBytes.vhd
-
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity SubBytes is
     port (
-        aMatrix: in std_logic_matrix(3 downto 0, 3 downto 0);
-        bMatrix: out std_logic_matrix(3 downto 0, 3 downto 0);
+        aMatrix: in std_logic_vector(127 downto 0);
+        bMatrix: out std_logic_vector(127 downto 0)
     );
 end entity SubBytes;
 
 architecture arch_SubBytes of SubBytes is
-    --- Component definition
-    component S_box is port(
-        byte_in : in std_logic_vector(7 downto 0);
-        byte_out : out std_logic_vector(7 downto 0)
-    );end component;
 
-    --- Function definition
-    function SubBytesTransformation(input_byte: std_logic_vector) return std_logic_vector is
-        variable output_byte: std_logic_vector(7 downto 0);
-    begin
-        S_box_instance: S_box port map(
-            byte_in => input_byte,
-            byte_out => output_byte
+    -- Component declaration
+    component S_box is
+        port (
+            BYTE_IN : in std_logic_vector(7 downto 0);
+            BYTE_OUT : out std_logic_vector(7 downto 0)
         );
-        return output_byte;
-    end function;
+    end component;
+    
+    -- Type declaration
+    type Matrix is array (0 to 3, 0 to 3) of std_logic_vector(7 downto 0);
+
+    -- Signal declaration
+    signal temp_byte : std_logic_vector(7 downto 0);
+    signal byte_out : std_logic_vector(7 downto 0);
+    
+    -- Function declaration
+    function hexaToMatrix(input_hex : std_logic_vector(127 downto 0)) return Matrix is
+        variable result : Matrix := (
+                                (x"00", x"00", x"00", x"00"),
+                                (x"00", x"00", x"00", x"00"),
+                                (x"00", x"00", x"00", x"00"),
+                                (x"00", x"00", x"00", x"00")
+                              );
+        variable index : integer := 0;    
+        begin
+            for col_k in 0 to 3 loop
+                for row_k in 0 to 3 loop   
+                    if index < input_hex'length then
+                        result(row_k,col_k) := std_logic_vector(unsigned(input_hex((7+index*7) downto (0+index*7))));
+                        index := index + 1;
+                    end if;
+                end loop;
+            end loop;
+        return result;    
+    end hexaToMatrix;
+
+    function matrixToHexa(input_matrix : Matrix) return std_logic_vector is
+        variable result : std_logic_vector(127 downto 0) := (others => '0');
+        variable index : integer := 0;    
+        begin
+            for col_k in 0 to 3 loop
+                for row_k in 0 to 3 loop   
+                    result((7+index*7) downto (0+index*7)) := input_matrix(row_k,col_k);
+                    index := index + 1;
+                end loop;
+            end loop;
+        return result;    
+    end matrixToHexa;
+    
 
 begin
-    process
+    S_box_inst: S_box port map (
+        BYTE_IN => temp_byte,
+        BYTE_OUT => byte_out
+    );
+
+    subBytes_process: process
         variable row, col: integer;
+        variable temp_matrix : Matrix := hexaToMatrix(aMatrix);
     begin
         for row in 0 to 3 loop
             for col in 0 to 3 loop
-                bMatrix(row, col) <= SubBytesTransformation(aMatrix(row, col));
+                temp_byte <= temp_matrix(row,col);
+                wait for 5 ns;
+                temp_matrix(row, col) := byte_out;
+                wait for 5 ns;
             end loop;
         end loop;
+        bMatrix <= matrixToHexa(temp_matrix);
     end process;
 end architecture arch_SubBytes;
---
+++ SubBytes_tb.vhd
@@ -0,0 +1,45 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity SubBytes_tb is
+end entity SubBytes_tb;
+
+architecture arch_SubBytes_tb of SubBytes_tb is
+    component SubBytes is port(
+        aMatrix: in std_logic_vector(127 downto 0);
+        bMatrix: out std_logic_vector(127 downto 0)
+    ); end component;
+
+    signal plain_text : std_logic_vector(127 downto 0); 
+    signal cipher_text : std_logic_vector(127 downto 0);
+
+    
+begin
+    SubBytes_instance: SubBytes port map(
+        aMatrix => plain_text,
+        bMatrix => cipher_text
+    );
+
+    simulation_rounds: process
+    begin
+        plain_text <= x"40BFABF406EE4D3042CA6B997A5C5816";
+        wait for 10 ns;
+        plain_text <= x"F265E8D51FD2397BC3B9976D9076505C";
+        wait for 10 ns;
+        plain_text <= x"FDF37CDB4B0C8C1BF7FCD8E94AA9BBF8";
+        wait for 10 ns;
+        plain_text <= x"ACD1EC9CA242E2C31F690F7AB704B90F";
+        wait for 10 ns;
+        plain_text <= x"A2616E5F44A54D39C029E20092B764E9";
+        wait for 10 ns;
+        plain_text <= x"2C4AF31432C3EFC9C8A9B87B252ECDA7";
+        wait for 10 ns;
+        plain_text <= x"CD4DC0137EB3BA1993B939FF2BD3BCF7";
+        wait for 10 ns;
+        plain_text <= x"E26DBB7D40D22134E3B7FDA26B9B077C";
+        wait for 10 ns;
+        plain_text <= x"41D7C6537D669140DD2F179D02ACC51B";
+        wait for 10 ns;       
+    end process;
+end architecture arch_SubBytes_tb;
