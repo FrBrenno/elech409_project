feb517a5e89bb1f251d73b61ab564045262222d4
FrBrenno <72556312+FrBrenno@users.noreply.github.com>
2023-12-19 23:49:42 +0100

    matrix input/output shiftrows

--- ShiftRows.vhd
+++ ShiftRows.vhd
@@ -1,62 +1,59 @@
 ----------------------------------------------------------------------------------
 -- Update : 15-12-2023
 ----------------------------------------------------------------------------------
-library IEEE, work;
+library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
+
+library work;
 use work.matrix_pkg.all;
 
 entity ShiftRows is
 Port ( 
-    input_vector : in std_logic_vector (127 downto 0);
-    output_vector : out std_logic_vector (127 downto 0)
+    input_matrix : in Matrix(0 to 3, 0 to 3);
+    output_matrix : out Matrix(0 to 3, 0 to 3)
 );end ShiftRows;
 
 architecture Behavioral of ShiftRows is
 
-    --- Creation of the matrix ---    
-    signal input_Matrix : Matrix(0 to 3, 0 to 3);
-    signal output_Matrix : Matrix(0 to 3, 0 to 3) := (
-                                (x"00", x"00", x"00", x"00"),
-                                (x"00", x"00", x"00", x"00"),
-                                (x"00", x"00", x"00", x"00"),
-                                (x"00", x"00", x"00", x"00")
-                              );
 begin   
-    -- Conversion of the input vector into a matrix
-    input_Matrix <= hexaToMatrix(input_vector);
-
     -- Shifting process
-    shifting : process(input_Matrix, output_Matrix)
+    shifting : process(input_matrix)
+        variable output_data : Matrix(0 to 3, 0 to 3) := (
+            (x"00", x"00", x"00", x"00"),
+            (x"00", x"00", x"00", x"00"),
+            (x"00", x"00", x"00", x"00"),
+            (x"00", x"00", x"00", x"00")
+          );
     begin
         for row in 0 to 3 loop
             for col in 0 to 3 loop
                 if row = 0 then
-                    output_Matrix(row,col) <= input_Matrix(row,col);
+                    output_data(row,col) := input_Matrix(row,col);
                 elsif row = 1 then
                     if col = 0 then
-                        output_Matrix(row,3) <= input_Matrix(row,col);
+                        output_data(row,3) := input_Matrix(row,col);
                     else
-                        output_Matrix(row,col-1) <= input_Matrix(row,col);
+                        output_data(row,col-1) := input_Matrix(row,col);
                     end if;
                 elsif row = 2 then
                     if col = 0 then
-                        output_Matrix(row,2) <= input_Matrix(row,col);
+                        output_data(row,2) := input_Matrix(row,col);
                     elsif col = 1 then
-                        output_Matrix(row,3) <= input_Matrix(row,col);
+                        output_data(row,3) := input_Matrix(row,col);
                     else
-                        output_Matrix(row,col-2) <= input_Matrix(row,col);
+                        output_data(row,col-2) := input_Matrix(row,col);
                     end if;
                 elsif row = 3 then
                     if col = 3 then
-                        output_Matrix(row,0) <= input_Matrix(row,col);
+                        output_data(row,0) := input_Matrix(row,col);
                     else
-                        output_Matrix(row,col+1) <= input_Matrix(row,col);
+                        output_data(row,col+1) := input_Matrix(row,col);
                     end if;                      
                 end if;
             end loop;        
         end loop;
-        output_vector <= matrixToHexa(output_Matrix);
+        output_matrix <= output_data;
     end process shifting;       
     
 end architecture Behavioral;
--- ShiftRows_tb.vhd
+++ ShiftRows_tb.vhd
@@ -3,66 +3,70 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 USE ieee.math_real.ALL;
 
+library work;
+use work.matrix_pkg.all;
+
 ENTITY ShiftRows_tb IS
 END ENTITY;
 
 ARCHITECTURE ben2 OF ShiftRows_tb IS
     COMPONENT ShiftRows IS PORT (
-        input_vector : IN STD_LOGIC_VECTOR (127 DOWNTO 0);
-        output_vector : OUT STD_LOGIC_VECTOR (127 DOWNTO 0)
+        input_matrix : IN Matrix(0 TO 3, 0 TO 3);
+        output_matrix : OUT Matrix(0 TO 3, 0 TO 3)
         );
     END COMPONENT;
 
     -- These are the internal wires
     SIGNAL plain_text : STD_LOGIC_VECTOR(127 DOWNTO 0);
     SIGNAL cipher_text : STD_LOGIC_VECTOR(127 DOWNTO 0);
+    SIGNAL output_data : Matrix(0 TO 3, 0 TO 3);
 
 BEGIN
     uut : ShiftRows PORT MAP(
-        input_vector => plain_text,
-        output_vector => cipher_text
+        input_matrix => hexaToMatrix(plain_text),
+        output_matrix => output_data
     );
 
     stim : PROCESS
     BEGIN
         plain_text <= x"090862BF6F28E3042C747FEEDA4A6A47";
-        WAIT FOR 5 ns;
-        ASSERT (cipher_text /= x"09287F476F746ABF2C4A6204DA08E3EE")
-        REPORT "1: Output is OK" SEVERITY note;
+        WAIT FOR 1 ps;
+        cipher_text <= matrixToHexa(output_data);
+        WAIT FOR 1 ps;
         ASSERT (cipher_text = x"09287F476F746ABF2C4A6204DA08E3EE")
-        REPORT "1: Output is not correct" SEVERITY warning;
+        REPORT "1: INCORRECT OUTPUT" SEVERITY warning;
         WAIT FOR 5 ns;
 
         plain_text <= x"894D9B03C0B512212E56883C6038534A";
-        WAIT FOR 5 ns;
-        ASSERT (cipher_text /= x"89B5884AC05653032E389B21604D123C")
-        REPORT "2: Output is OK" SEVERITY note;
+        WAIT FOR 1 ps;
+        cipher_text <= matrixToHexa(output_data);
+        WAIT FOR 1 ps;
         ASSERT (cipher_text = x"89B5884AC05653032E389B21604D123C")
-        REPORT "2: Output is not correct" SEVERITY warning;
+        REPORT "2: INCORRECT OUTPUT" SEVERITY warning;
         WAIT FOR 5 ns;
 
         plain_text <= x"540D10B9B3FE64AF68B0611ED6D3EA41";
-        WAIT FOR 5 ns;
-        ASSERT (cipher_text /= x"54FE6141B3B0EAB968D310AFD60D641E")
-        REPORT "3: Output is OK" SEVERITY note;
+        WAIT FOR 1 ps;
+        cipher_text <= matrixToHexa(output_data);
+        WAIT FOR 1 ps;
         ASSERT (cipher_text = x"54FE6141B3B0EAB968D310AFD60D641E")
-        REPORT "3: Output is not correct" SEVERITY warning;
+        REPORT "3: INCORRECT OUTPUT" SEVERITY warning;
         WAIT FOR 5 ns;
 
         plain_text <= x"913ECEDE3A2C982EC0F976DAA9F25676";
-        WAIT FOR 5 ns;
-        ASSERT (cipher_text /= x"912C76763AF956DEC0F2CE2EA93E98DA")
-        REPORT "4: Output is OK" SEVERITY note;
+        WAIT FOR 1 ps;
+        cipher_text <= matrixToHexa(output_data);
+        WAIT FOR 1 ps;
         ASSERT (cipher_text = x"912C76763AF956DEC0F2CE2EA93E98DA")
-        REPORT "4: Output is not correct" SEVERITY warning;
+        REPORT "4: INCORRECT OUTPUT" SEVERITY warning;
         WAIT FOR 5 ns;
 
         plain_text <= x"3AEF9FCF1B06E312BAA598634FA9431E";
-        WAIT FOR 5 ns;
-        ASSERT (cipher_text /= x"3A06981E1BA543CFBAA99F124FEFE363")
-        REPORT "5: Output is OK" SEVERITY note;
+        WAIT FOR 1 ps;
+        cipher_text <= matrixToHexa(output_data);
+        WAIT FOR 1 ps;
         ASSERT (cipher_text = x"3A06981E1BA543CFBAA99F124FEFE363")
-        REPORT "5: Output is not correct" SEVERITY warning;
+        REPORT "5: INCORRECT OUTPUT" SEVERITY warning;
         WAIT FOR 5 ns;
     END PROCESS;
 END ben2;
