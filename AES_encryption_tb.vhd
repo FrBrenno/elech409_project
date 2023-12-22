ccfcdccdb5fddcfec51c5fa8185b6666e3430eb8
FrBrenno <72556312+FrBrenno@users.noreply.github.com>
2023-12-22 14:45:18 +0100

    aes rounds working, missing last round logic

--- AES_encryption_tb.vhd
+++ AES_encryption_tb.vhd
@@ -7,7 +7,7 @@ END AES_encryption_tb;
 ARCHITECTURE tb_arch OF AES_encryption_tb IS
     COMPONENT AES_encryption
         PORT (
-            plain_text, key : IN STD_LOGIC_VECTOR(127 DOWNTO 0);
+            plain_text : IN STD_LOGIC_VECTOR(127 DOWNTO 0);
             clk : IN STD_LOGIC;
             rst : IN STD_LOGIC;
             cipher_text : OUT STD_LOGIC_VECTOR(127 DOWNTO 0);
@@ -15,7 +15,7 @@ ARCHITECTURE tb_arch OF AES_encryption_tb IS
         );
     END COMPONENT;
 
-    SIGNAL plain_text, key, cipher_text_output : STD_LOGIC_VECTOR(127 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL plain_text, cipher_text_output : STD_LOGIC_VECTOR(127 DOWNTO 0) := (OTHERS => '0');
     SIGNAL clk: STD_LOGIC := '0';
     signal rst : STD_LOGIC := '1'; 
     SIGNAL done : STD_LOGIC;
@@ -24,7 +24,6 @@ BEGIN
     uut : AES_encryption
     PORT MAP(
         plain_text => plain_text,
-        key => key,
         clk => clk,
         rst => rst,
         cipher_text => cipher_text_output,
@@ -46,8 +45,7 @@ BEGIN
     stimulus : PROCESS
     BEGIN
         plain_text <= x"6BC1BEE22E409F96E93D7E117393172A";
-        key <= x"2B7E151628AED2A6ABF7158809CF4F3C";
-        WAIT FOR 800 ns;
+        WAIT FOR 500 ns;
         ASSERT (cipher_text_output = x"85539F4136AD7E3A35407A244C60C16D")
         REPORT "1: INCORRECT OUTPUT\n" SEVERITY note;
         WAIT FOR 5 ns;
